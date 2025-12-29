796e0a638c8b34c5e002196bd86bcd6685f3baae
alexmps2003 <126064822+alexmps2003@users.noreply.github.com>
2025-12-29 16:28:30 +0530

    Testing if Git preserves folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">This is the Home Page</h1>
+      <h1 className="text-3xl font-bold">
+        This is the Home Page git is working
+      </h1>
       <Link href="/dashboard" className="text-blue-500 underline mt-4">
         Go to Dashboard
       </Link>

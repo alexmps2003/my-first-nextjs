c4da48f68cc783a38cd829b59bff49af811b8e65
alexmps2003 <126064822+alexmps2003@users.noreply.github.com>
2025-12-29 17:40:48 +0530

    side bar added to dashboard

---
+++ app/ui/dashboard/sidenav.tsx
@@ -0,0 +1,10 @@
+export default function SideNav() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-gray-50">
+      <p>Side Navigation</p>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        {/* Navigation links will go here in Chapter 5 */}
+      </div>
+    </div>
+  );
+}

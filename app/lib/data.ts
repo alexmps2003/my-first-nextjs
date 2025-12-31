8f7f6c4fe2bc59f1f7e4706ea99dbb19186a0b42
alexmps2003 <126064822+alexmps2003@users.noreply.github.com>
2025-12-31 17:44:51 +0530

    Implement route group and stream RevenueChart with Suspense

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,6 +10,10 @@ import { formatCurrency } from "./utils";
 
 export async function fetchRevenue() {
   try {
+    //artificial delay to simulate network latency
+    console.log("Fetching revenue data...");
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<Revenue>`SELECT * FROM revenue`;
     return data.rows;
   } catch (error) {
